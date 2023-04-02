869f62698b4fde914d61d72deac227f240a9fd4c
shrsyc <shrsyc@gmail.com>
2023-04-03 02:46:24 +0530

    add first last and print

--- Linked_List/single_LL/addNode.java
+++ Linked_List/single_LL/addNode.java
@@ -15,7 +15,7 @@ public class addNode {
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data){
+    public void addFirst(int data){     
         Node newnode = new Node(data);
         if(head==null){
             head=tail=newnode;
@@ -25,11 +25,38 @@ public class addNode {
         head=newnode;
     }
 
+
+    public void addLast(int data){
+        Node newnode = new Node(data);
+        if(head==null){
+            head=tail=newnode;
+            return;
+        }
+        tail.next=newnode;
+        tail=newnode;
+    }
+
+
+    public void printLL(){
+        if(head==null){
+            System.out.println("Linked List is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+
     public static void main(String[] args) {
         addNode a = new addNode();
         a.addFirst(1);
         a.addFirst(2);
-
+        a.addLast(0);
+        a.printLL();
     }
     
 }
